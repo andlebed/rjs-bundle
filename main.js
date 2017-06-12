f16f68aa814585109c96cf7d33c93a4ac450074d
las12425 <las12425@mail.ru>
2017-06-12 22:49:31 +0300

    Update 2017-06-12T19:49:30.341Z

--- main.js
+++ main.js
@@ -3,12 +3,19 @@
 // import {ValidationEngine} from 'aurelia-validation';
 // import {I18N} from 'aurelia-i18n';
 // import {VirtualRepeat} from 'aurelia-ui-virtualization';
+import 'regenerator-runtime';
 
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
     .plugin('aurelia-validation')
+    .plugin('aurelia-plugins-google-recaptcha', config => {
+      config.options({
+        hl: 'en',
+        siteKey: '6LcddxgTAAAAAMmkEMa1Vrp6TNcZG8kMMkcn-VCK'
+      });
+    })
     .feature('bootstrap-validation');
     // .plugin('aurelia-ui-virtualization')
     // .plugin('aurelia-i18n')
